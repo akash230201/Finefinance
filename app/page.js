cf3d6904b212566c18f653d971fc1fea8c2cf1aa
Akash <akashmondal739@gmail.com>
2025-05-30 01:05:31 +0530

    V 0.5.30

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>FineFinance hi</div>;
+  return <Button variant="destructive">FineFinance hi</Button>;
 }
