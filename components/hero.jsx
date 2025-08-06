5d3ab492c4dd79b3cc087144fc97bd1c3e7fb82e
akash230201 <141403797+akash230201@users.noreply.github.com>
2025-08-06 14:15:49 +0530

    V 0.8.6.2025-landing page hero image

--- components/hero.jsx
+++ components/hero.jsx
@@ -3,8 +3,32 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import React, { useRef, useEffect } from "react";
 
 const HeroSection = () => {
+  const imageRef = useRef();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const imageElement = imageRef.current;
+
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100; // Adjust this value as needed
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="pb-20 px-4">
       <div className="container mx-auto text-center">
@@ -28,8 +52,8 @@ const HeroSection = () => {
           </Link>
         </div>
       </div>
-      <div>
-        <div>
+      <div className="hero-image-wrapper mt-5 md:mt-0">
+        <div ref={imageRef} className="hero-image">
           <Image
             src="/banner.png"
             alt="Hero Image"
